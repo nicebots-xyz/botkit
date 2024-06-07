c3d5eeb3040083d5104ee53ab3c252bd528c2585
Paillat <me@paillat.dev>
2024-06-07 11:03:35 +0200

    :bug: Allow for .yml and not only .yaml

--- src/config/bot_config.py
+++ src/config/bot_config.py
@@ -1,5 +1,14 @@
 import yaml
+import os
+
 from typing import Any
 
-with open("config.yaml", "r", encoding="utf-8") as f:
+if os.path.exists("config.yaml"):
+    path = "config.yaml"
+elif os.path.exists("config.yml"):
+    path = "config.yml"
+else:
+    raise FileNotFoundError("Configuration file not found")
+
+with open(path, "r", encoding="utf-8") as f:
     config: dict[str, dict[str, Any]] = yaml.safe_load(f)
