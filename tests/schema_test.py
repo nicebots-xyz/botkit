a995b164491b1c92c8f317ebbc8ed3b09393bd08
Paillat <me@paillat.dev>
2024-06-30 21:19:25 +0200

    feat(utils): add __init__.py and extensions.py files to the utils directory
    fix(tests): remove sample_test.py and add schema_test.py
    
    The __init__.py file is added to the utils directory to make it a package and allow importing modules from it. The extensions.py file is added to the utils directory to provide additional utility functions.
    
    The sample_test.py file is removed from the tests directory as it is no longer needed. The schema_test.py file is added to the tests directory to test the functionality of the schema validation for extensions.

---
+++ tests/schema_test.py
@@ -0,0 +1,17 @@
+from glob import iglob
+from os.path import splitext, basename
+import importlib
+
+from src.utils import validate_module
+
+
+def test_ext_schemas():
+    for ext in iglob("src/extensions/*"):
+        name = splitext(basename(ext))[0]
+        module = importlib.import_module(f"src.extensions.{name}")
+        validate_module(module)
+        del module
+
+
+if __name__ == '__main__':
+    test_ext_schemas()
