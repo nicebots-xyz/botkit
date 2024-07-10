989b7862c98bdfb9c087560057c3c79feae4603c
Paillat <me@paillat.dev>
2024-07-10 18:01:30 +0200

    :sparkles: Add `mention_command` util

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .extensions import validate_module, unzip_extensions
+from .misc import mention_command
 
 __all__ = ["validate_module", "unzip_extensions"]
---
+++ src/utils/misc.py
@@ -0,0 +1,9 @@
+import discord
+
+
+def mention_command(*command: str, bot: discord.Bot) -> str:
+    command = " ".join(command)
+    command = bot.get_application_command(command)
+    if isinstance(command, discord.SlashCommand):
+        return command.mention
+    raise ValueError("Command not found")
